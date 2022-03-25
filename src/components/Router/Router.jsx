328371599bb30e88a6d119235a023e368cae98f8
DevIdol <devidol.mm@gmail.com>
2022-03-25 08:15:58 +0630

    Router

---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default Router;
