a8dae8c95729f89882095d531cd0710fffd41391
DevIdol <devidol.mm@gmail.com>
2022-03-26 16:39:55 +0630

    Modified

---
+++ src/Components/Home/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section className="contact-section">
+      <Title
+        title="TECHNICAL SKILLS"
+        icon={<RiMedalFill className={classes.icon} />}
+        className={classes.underline}
+      />
+    </section>
+  );
+};
+
+export default Contact;
