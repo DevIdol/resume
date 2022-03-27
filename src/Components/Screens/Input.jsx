ab56302d386595ca03b2324fa528eb63d9e2da54
DevIdol <devidol.mm@gmail.com>
2022-03-27 09:08:53 +0630

    Contact

--- src/Components/Screens/Input.jsx
+++ src/Components/Screens/Input.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import classes from "./Input.module.css";
 
-const Input = ({ type, name, placeholder }) => {
+const Input = ({ type, name, placeholder, className }) => {
   return (
     <input
-      className={classes.input}
+      className={`${classes.input} ${className}`}
       type={type || "text"}
       name={name}
       placeholder={placeholder}
