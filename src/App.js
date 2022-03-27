04f3197b6d34ad5ee0471ae7f26eadb17edf400d
DevIdol <devidol.mm@gmail.com>
2022-03-27 15:33:58 +0630

    PortFolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import NavBar from './Components/NavBar/NavBar'
+import Portfolio from './Components/PortFolio/Portfolio'
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
         <Route path="portfolio" element={<NavBar />}>
-          <Route index element={<h1>PortFolio Page</h1>} />
+          <Route index element={<Portfolio />} />
         </Route>
         <Route path="blog" element={<NavBar />}>
           <Route index element={<h1>Blog Page</h1>} />
---
+++ src/Components/PortFolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div style={{paddingTop: "100px", textAlign: "center"}}>
+            <h1>To Be Continued PortFolio Page</h1>
+        </div>
+    );
+};
+
+export default Portfolio;
