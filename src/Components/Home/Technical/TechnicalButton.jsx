6ef7c1dee9cf167384c8517f404f31782d292042
DevIdol <devidol.mm@gmail.com>
2022-03-27 09:22:28 +0630

    Modified

--- src/Components/Home/Technical/TechnicalButton.jsx
+++ src/Components/Home/Technical/TechnicalButton.jsx
@@ -3,36 +3,48 @@ import Button from "../../Screens/Button";
 import BackEnd from "./BackEnd";
 import FrontEnd from "./FrontEnd";
 import OtherSkill from "./OtherSkill";
-import classes from "./TechnicalButton.module.css"
+import classes from "./TechnicalButton.module.css";
 
 const TechnicalButton = () => {
-  const [skill, setSkill] = useState(<FrontEnd/>);
+  const [skill, setSkill] = useState(<FrontEnd />);
   const [isActive, setIsActive] = useState("front");
   return (
     <Fragment>
       <div className={classes.btn}>
         <Button
-          className={isActive === "front" ? classes.active : ""}
+          className={
+            isActive === "front"
+              ? `${classes.button} ${classes.active}`
+              : classes.button
+          }
           onClick={() => {
-            setSkill(<FrontEnd/>);
+            setSkill(<FrontEnd />);
             setIsActive("front");
           }}
         >
           FrontEnd
         </Button>
         <Button
-          className={isActive === "back" ? classes.active : ""}
+          className={
+            isActive === "back"
+              ? `${classes.button} ${classes.active}`
+              : classes.button
+          }
           onClick={() => {
-            setSkill(<BackEnd/>);
+            setSkill(<BackEnd />);
             setIsActive("back");
           }}
         >
           BackEnd
         </Button>
         <Button
-          className={isActive === "other" ? classes.active : ""}
+          className={
+            isActive === "other"
+              ? `${classes.button} ${classes.active}`
+              : classes.button
+          }
           onClick={() => {
-            setSkill(<OtherSkill/>);
+            setSkill(<OtherSkill />);
             setIsActive("other");
           }}
         >
