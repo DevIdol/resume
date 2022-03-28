2edc4bf06eac178865a681f78639522971e87b5d
DevIdol <devidol.mm@gmail.com>
2022-03-28 12:01:10 +0630

    Blog

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -14,6 +14,16 @@ const Blog = () => {
       <br />
       <br />
       <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
