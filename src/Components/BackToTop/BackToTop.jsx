66c1cb7687240460cc4a494fb93356aa045f4d7c
DevIdol <devidol.mm@gmail.com>
2022-03-28 11:15:18 +0630

    BackToTop

---
+++ src/Components/BackToTop/BackToTop.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BackToTop = () => {
+  return (
+    <div>
+      <a href="#" className={classes["to-top"]}>
+        Back to top
+      </a>
+    </div>
+  );
+};
+
+export default BackToTop;
