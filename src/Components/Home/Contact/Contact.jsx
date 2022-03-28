24c6bd76c6732530888d856b7206ecb75ed8851b
DevIdol <devidol.mm@gmail.com>
2022-03-28 09:54:57 +0630

    Contact

--- src/Components/Home/Contact/Contact.jsx
+++ src/Components/Home/Contact/Contact.jsx
@@ -1,19 +1,12 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../../../Context/ThemeContext";
-import dark from "../../../assets/dark.gif";
-import light from "../../../assets/light.gif";
+import React from "react";
 import classes from "./Contact.module.css";
 import ContactTitle from "./ContactTitle";
 import ContactForm from "./ContactForm";
 
 const Contact = () => {
-  const [{ theme }] = useContext(ThemeContext);
-  const bgImg = theme === "dark" ? dark : light;
   return (
     <section className={`${classes["contact-section"]}`}>
-      <img src={bgImg} alt="" />
-      <div className={classes.overlay}></div>
-      <ContactTitle />
+      <ContactTitle/>
       <ContactForm />
     </section>
   );
--- src/Components/Home/Contact/ContactTitle.jsx
+++ src/Components/Home/Contact/ContactTitle.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import { FaRegStar } from "react-icons/fa";
 import Title from "../../Screens/Title";
-import classes from "./ContactTitle.module.css"
+import classes from "./ContactTitle.module.css";
 const ContactTitle = () => {
-    return (
-        <div className={`${classes["contact-title"]}`}>
-        <Title
-          title="CONTACT ME"
-          icon={<FaRegStar className={classes.icon} />}
-          className={classes.underline}
-        />
-      </div>
-    );
+  return (
+    <div className={`${classes["contact-title"]}`}>
+      <Title
+        title="CONTACT ME"
+        icon={<FaRegStar className={classes.icon} />}
+        className={classes.underline}
+      />
+    </div>
+  );
 };
 
 export default ContactTitle;
