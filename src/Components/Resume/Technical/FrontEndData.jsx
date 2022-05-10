7fcd1ff27d09dab97d8b16b165521fb153c97c3b
DevIdol <devidol.mm@gmail.com>
2022-05-10 13:32:10 +0630

    Modified Resume

--- src/Components/Resume/Technical/FrontEndData.jsx
+++ src/Components/Resume/Technical/FrontEndData.jsx
@@ -41,20 +41,19 @@ const FrontEndData = [
     color: "#fff",
     bgcolor: "#0000ff",
   },
-
   {
     id: 7,
-    name: "MUI4/5",
-    progress: 60,
-    color: "#fff",
-    bgcolor: "#0000ff",
-  },
-  {
-    id: 8,
     name: "Bootstrap4/5",
     progress: 80,
     color: "#fff",
     bgcolor: "#7952b3",
+  },
+  {
+    id: 8,
+    name: "MUI4/5",
+    progress: 60,
+    color: "#fff",
+    bgcolor: "#0000ff",
   },
 ];
 export const getFrontEndData = () => FrontEndData;
