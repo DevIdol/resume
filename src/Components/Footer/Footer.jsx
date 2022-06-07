b8d589fc2b3f721056afec72a478c47f3b2777b5
DevIdol <devidol.mm@gmail.com>
2022-06-07 09:31:42 +0630

    Modified

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import SocialMedia from "../Resume/About/SocialMedia";
-import logo from "../../assets/light.png";
 import classes from "./Footer.module.css";
 import BackToTop from "../BackToTop/BackToTop";
 
 const Footer = ({href}) => {
   return (
     <footer className={classes.footer}>
-      <div className={classes.logo}>
-        <img src={logo} alt="" />
-      </div>
       <div className={classes["footer-content"]}>
         <h6>Hey There, I Am</h6>
         <h5>Web And Mobile Application Developer</h5>
